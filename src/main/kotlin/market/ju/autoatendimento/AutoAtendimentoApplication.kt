8d821bc3ea65da8f0a25aa4282070a9c7426e60b
diegoalvarengarodrigues <diegoalvarengarodrigues@gmail.com>
2023-07-31 05:38:43 -0300

    inicio

---
+++ src/main/kotlin/market/ju/autoatendimento/AutoAtendimentoApplication.kt
@@ -0,0 +1,11 @@
+package market.ju.autoatendimento
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutoAtendimentoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutoAtendimentoApplication>(*args)
+}
---
+++ src/test/kotlin/market/ju/autoatendimento/AutoAtendimentoApplicationTests.kt
@@ -0,0 +1,13 @@
+package market.ju.autoatendimento
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutoAtendimentoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
